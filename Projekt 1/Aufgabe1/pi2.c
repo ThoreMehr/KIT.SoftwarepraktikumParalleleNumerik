5b27fabaaf8d14e5335d7a783890d399253b89de
thore.mehr@gmail.com <thore.mehr@gmail.com>
2016-12-16 17:20:09 +0100

    pi

--- Projekt 1/Aufgabe1/pi2.c
+++ Projekt 1/Aufgabe1/pi2.c
@@ -14,19 +14,6 @@ int main(int argc, char* argv[]) {
 	
 	struct timeval time1,time2;
 	struct timezone zone;
-	gettimeofday(&time1,&zone);
-	sum = 0.0;
-	step = 1.0 / (double) num_steps;
-	for (i = 1; i <= num_steps; i++) {
-		x = (i-0.5) * step;
-		sum +=4.0/(1.0+x*x);
-	}
-	pi = step * sum;
-	gettimeofday(&time2,&zone);
-	printf("PI: %f\n", pi);
-	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;	
-	printf("time:%f s\n",t);
-
 //para1
 	gettimeofday(&time1,&zone);
 	sum = 0.0;
@@ -42,25 +29,7 @@ int main(int argc, char* argv[]) {
 	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;	
 	printf("time:%f s\n",t);
 
-//para2
-	gettimeofday(&time1,&zone);
-	sum = 0.0;
-	step = 1.0 / (double) num_steps;
 
-	#pragma omp parallel for private(x)
-	for (i = 1; i <= num_steps; i++) {
-		x = (i-0.5) * step;
-		#pragma omp critical
-		{
-			sum += 4.0/(1.0+x*x);
-		}
-	}
-	pi = step * sum;
-	
-	gettimeofday(&time2,&zone);
-	printf("PI: %f\n", pi);
-	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;	
-	printf("time:%f s\n",t);
 
 	return 0;
 }
