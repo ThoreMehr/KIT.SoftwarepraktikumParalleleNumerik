65b60df8781d8aea6173296e765603441218fe60
thore.mehr@gmail.com <thore.mehr@gmail.com>
2016-12-17 15:12:24 +0100

    matrix mul

---
+++ Projekt 1/Aufgabe2/matrixmul_red.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#ifndef N
+	#define N 500
+#endif
+	int main(int argc, char* argv[]) {
+		//read the two matrixes from file
+		FILE * pFile;
+   		pFile = fopen (argv[1], "r");
+   		//float m1[N][N];
+   		float* m1=malloc(sizeof(float)*N*N);//output matrix
+  		fread (m1 , sizeof(float), N*N, pFile);
+  		fclose (pFile);
+   		pFile = fopen (argv[2], "r");
+//   		float m2[N][N];
+		float* m2=malloc(sizeof(float)*N*N);//output matrix
+  		fwrite (m2 , sizeof(float), N*N, pFile);
+  		fclose (pFile);
+  		float* m3=malloc(sizeof(float)*N*N);//output matrix
+  		//float m3[N][N];
+  		int i=0,j=0,k=0;
+  		
+		struct timeval time1,time2;
+		struct timezone zone;	
+		gettimeofday(&time1,&zone);
+		float sum;
+  		for ( i = 0; i < N; i++) {
+        	for ( k = 0; k < N; k++) {
+				sum=0.0;
+				#pragma omp parallel for reduction(+:sum)
+            	for (j = 0; j < N; j++) {
+            	    sum += m1[i*N+k] * m2[k*N+j];
+            	}
+				m3[i*N+j]=sum;
+        	}
+    	}
+    	gettimeofday(&time2,&zone);
+    	
+		float t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;	
+		printf("time:%f s\n",t);
+    
+   		pFile = fopen (argv[3], "wb");
+	  	fwrite (m3 , sizeof(float), N*N, pFile);
+  		fclose (pFile);		
+}
+
