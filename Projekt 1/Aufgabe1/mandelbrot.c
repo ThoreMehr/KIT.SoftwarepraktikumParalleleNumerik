63f27b3b915bf20245f130ddfaf419dfae7d5b8b
thore.mehr@gmail.com <thore.mehr@gmail.com>
2016-12-18 20:08:41 +0100

    mandelbrot schedule

--- Projekt 1/Aufgabe1/mandelbrot.c
+++ Projekt 1/Aufgabe1/mandelbrot.c
@@ -44,19 +44,12 @@ int main() {
 	
 	T = (int*) malloc(sizeof(int)*N*N);
 	float tp;
-	printf("Starting calculation for N=%d... with %d threads", N,omp_get_max_threads());
+	printf("Starting calculation for N=%d... with %d threads\n", N,omp_get_max_threads());
 	struct timeval time1,time2;
 	struct timezone zone;	
 	gettimeofday(&time1,&zone);
-	#ifndef scheduleType
-		#pragma omp parallel for private(j,z,kappa,k) shared(T)
-	#else
-		#ifndef chunksize
-	 		#pragma omp parallel for private(j,z,kappa,k) shared(T) schedule(scheduleType)
-		#else
-			#pragma omp parallel for private(j,z,kappa,k) shared(T) schedule(scheduleType,chunksize)
-		#endif
-	#endif
+			
+	#pragma omp parallel for private(j,z,kappa,k) shared(T) schedule(runtime)
 	for (i=0; i<N; i++) {
 		for (j=0; j<N; j++) {
 			z.re = kappa.re = (4.0*(i-N/2))/N;
