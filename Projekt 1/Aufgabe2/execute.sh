2fd67a489520bfbb6ce7435730aadfc4bdfcc207
thore.mehr@gmail.com <thore.mehr@gmail.com>
2016-12-18 11:47:41 +0100

    execute script

---
+++ Projekt 1/Aufgabe2/execute.sh
@@ -0,0 +1,8 @@
+./matrixmul_lin_N$2_$1_icc
+./matrixmul_red_N$2_$1_icc
+./matrixmul_in_N$2_$1_icc
+./matrixmul_out_N$2_$1_icc
+./matrixmul_lin_N$2_$1_gcc
+./matrixmul_red_N$2_$1_gcc
+./matrixmul_in_N$2_$1_gcc
+./matrixmul_out_N$2_$1_gcc
