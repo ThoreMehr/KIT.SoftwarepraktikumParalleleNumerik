74a65268c2b9433563358af1cec710caeae2c79f
thore.mehr@gmail.com <thore.mehr@gmail.com>
2017-01-17 13:35:41 +0100

    P2A2a

---
+++ Projekt 2/Aufgabe2/a2.cu
@@ -0,0 +1,60 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#ifndef N
+	#define N 1000000
+#endif
+int main() {
+	cudaSetDevice(0);
+	srand(time(NULL));
+	size_t size=N*sizeof(int);
+	int* c_a=(int*)malloc(size);
+	for(int i=0;i<N;i++){
+		c_a[i]=rand()
+	}
+	int* c_b=(int*)malloc(size);
+	struct timeval time1,time2;
+	struct timezone zone;
+
+	gettimeofday(&time1,&zone);
+	for(int i=0;i<N;i++){
+		c_b[i]=c_a[i];
+	}
+
+	gettimeofday(&time2,&zone);
+	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;	
+	printf("time:%f s\n",t);
+
+	int* d_a;
+	cudaMalloc(&d_a,size);
+	int* d_b;
+	cudaMalloc(&d_b,size);
+	
+	gettimeofday(&time1,&zone);
+
+	cudaMemcpy(d_a,c_a,size,cudaMemcpyHostToDevice);
+
+	gettimeofday(&time2,&zone);
+	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;	
+	printf("time:%f s\n",t);
+
+	gettimeofday(&time1,&zone);
+	cudaMemcpy(d_b,d_a,size,cudaMemcpyDeviceToDevice);
+	
+	gettimeofday(&time2,&zone);
+	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;	
+	printf("time:%f s\n",t);
+
+	gettimeofday(&time1,&zone);
+
+	cudaMemcpy(c_b,d_b,size,cudaMemcpyDeviceToHost);
+
+	gettimeofday(&time2,&zone);
+	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;	
+	printf("time:%f s\n",t);
+
+	cudaFree(d_a);
+	cudaFree(d_b);
+	return 0;
+}
---
+++ Projekt 2/Aufgabe2/compileRun.sh
@@ -0,0 +1,2 @@
+nvcc --compiler-bindir=/usr/bin/gcc-4.4 -lstdc++ a2.cu -o a2
+./a2
