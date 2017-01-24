6b6f4779e643072285630b176873aaa7b861b03d
Praktikum spc01 <prakspc01@i82sn02.irf.uni-karlsruhe.de>
2017-01-24 16:20:52 +0100

    a2a

--- Projekt 2/Aufgabe2/a2a.cu
+++ Projekt 2/Aufgabe2/a2a.cu
@@ -2,15 +2,24 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <stdlib.h>
+#define Mega 1000000
+
+__global__ void Memcopy_add(int* A,int* B,int add){
+	int i =threadIdx.x;
+	B[i]=A[i]+add;
+}
+
 int main() {
 	cudaSetDevice(0);
 	srand(time(NULL));
 	size_t size=__N__*sizeof(int);
 	int* c_a=(int*)malloc(size);
 	for(int i=0;i<__N__;i++){
-		c_a[i]=rand();
+		c_a[i]=rand()%1000;
 	}
+	printf("%d\n",c_a[1]);
 	int* c_b=(int*)malloc(size);
+	int* c_c=(int*)malloc(size);
 	struct timeval time1,time2;
 	struct timezone zone;
 	float t=0.0,r=0.0;
@@ -19,12 +28,11 @@ int main() {
 	for(int i=0;i<__N__;i++){
 		c_b[i]=c_a[i];
 	}
-
 	gettimeofday(&time2,&zone);
 	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;
 	r=(size/1000000000.0)/t;	
-	printf("time:%f s, rate:%f GB/s\n",t,r);
-
+	printf("RAM->RAM time:%f s, rate:%f GB/s\n",t,r);
+	printf("%d\n",c_b[1]);
 	int* d_a;
 	cudaMalloc(&d_a,size);
 	int* d_b;
@@ -37,25 +45,32 @@ int main() {
 	gettimeofday(&time2,&zone);
 	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;
 	r=(size/1000000000.0)/t;	
-	printf("time:%f s, rate:%f GB/s\n",t,r);
+	printf("RAM->GPU time:%f s, rate:%f GB/s\n",t,r);
 
 	gettimeofday(&time1,&zone);
-	cudaMemcpy(d_b,d_a,size,cudaMemcpyDeviceToDevice);
+	//cudaMemcpy(d_b,d_a,size,cudaMemcpyDeviceToDevice);
+	Memcopy_add<<<1,__N__>>>(d_a,d_b,0);
 	
 	gettimeofday(&time2,&zone);
 	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;
 	r=(size/1000000000.0)/t;	
-	printf("time:%f s, rate:%f GB/s\n",t,r);
-
+	printf("GPU->GPU time:%f s, rate:%f GB/s\n",t,r);
 	gettimeofday(&time1,&zone);
 
-	cudaMemcpy(c_b,d_b,size,cudaMemcpyDeviceToHost);
+	cudaMemcpy(c_c,d_b,size,cudaMemcpyDeviceToHost);
 
 	gettimeofday(&time2,&zone);
 	t=((time2.tv_usec-time1.tv_usec)+(time2.tv_sec-time1.tv_sec)*1000000)/1000000.0;
 	r=(size/1000000000.0)/t;	
-	printf("time:%f s, rate:%f GB/s\n",t,r);
-
+	printf("GPU->RAM time:%f s, rate:%f GB/s\n",t,r);
+	printf("%d\n",c_c[1]);
+	for(int i=0;i<__N__;i++){
+		if(c_c[i]-2!=c_a[i]){
+			printf("%d||%d\n",c_a[i],c_c[i]);
+			printf("error at %d\n",i);
+			break;
+		}
+	}
 	cudaFree(d_a);
 	cudaFree(d_b);
 	return 0;
---
+++ Projekt 2/Aufgabe2/test.cu
@@ -0,0 +1,83 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+__global__ void Memcpy(int* target,int* source){
+	int i=threadIdx.x;
+	target[i]=source[i];
+}
+__global__ void Memcpyadd(int* target,int*source,int add){
+	int i=threadIdx.x;
+	target[i]=source[i]+add;
+}
+#define Mega 1000000
+#define Kilo 1000
+int main(){
+	srand(time(NULL));
+	size_t N=100*Mega;
+	size_t size=N*sizeof(int);
+
+	struct timeval t1,t2;
+	struct timezone z;
+	float t,r;
+	
+	int* c_a=(int*)malloc(size);
+	for(int i=0;i<N;i++){
+		c_a[i]=rand()%10000;
+	}	
+	int* d_a;
+	cudaMalloc(&d_a,size);
+
+	gettimeofday(&t1,&z);
+
+	//copy c_a to Device
+	cudaMemcpy(d_a,c_a,size,cudaMemcpyHostToDevice);
+
+	gettimeofday(&t2,&z);
+	t=((t2.tv_usec-t1.tv_usec)+(t2.tv_sec-t1.tv_sec)*1000000)/1000000.0;
+	r=(size/1000000000.0)/t;
+	printf("copy to device time: %f,rate: %f GB/s\n",t,r);
+
+	//c_b for testing	
+	int* c_b=(int*)malloc(size);
+	cudaMemcpy(c_b,d_a,size,cudaMemcpyDeviceToHost);
+
+	int* d_b;
+	cudaMalloc(&d_b,size);
+
+	gettimeofday(&t1,&z);
+	//copy d_a in d_b
+	cudaMemcpy(d_b,d_a,size,cudaMemcpyDeviceToDevice);
+
+	//Memcpy<<<1,N>>>(d_b,d_a);
+
+	gettimeofday(&t2,&z);
+	t=((t2.tv_usec-t1.tv_usec)+(t2.tv_sec-t1.tv_sec)*1000000)/1000000.0;
+	r=(size/1000000000.0)/t;
+	printf("copy on device time: %f,rate: %f GB/s\n",t,r);
+
+   	int* c_c=(int*)malloc(size);
+
+	gettimeofday(&t1,&z);
+	//copy d_b from device to c_c
+   	cudaMemcpy(c_c,d_b,size,cudaMemcpyDeviceToHost);
+
+	gettimeofday(&t2,&z);
+	t=((t2.tv_usec-t1.tv_usec)+(t2.tv_sec-t1.tv_sec)*1000000)/1000000.0;
+	r=(size/1000000000.0)/t;
+	printf("copy from device time: %f,rate: %f GB/s\n",t,r);
+//checking correctness
+   	for(int i=0;i<N;i++){
+   		if(c_b[i]!=c_a[i]){
+		printf("error in c_b at %d\n",i);
+		break;
+		}
+   	}
+   	for(int i=0;i<N;i++){
+   		if(c_c[i]!=c_a[i]){
+		printf("error in c_c at %d\n",i);
+		break;
+		}
+   	}
+}
