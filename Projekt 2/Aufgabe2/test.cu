9ba10a2368efe9c16db0da892a741a7a4cbe9ea9
Praktikum spc01 <prakspc01@i82sn02.irf.uni-karlsruhe.de>
2017-01-31 15:51:55 +0100

    a2a

--- Projekt 2/Aufgabe2/test.cu
+++ Projekt 2/Aufgabe2/test.cu
@@ -2,20 +2,21 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <stdlib.h>
+#include <omp.h>
 
-__global__ void Memcpy(int* target,int* source){
-	int i=threadIdx.x;
-	target[i]=source[i];
+__global__ void Memcpy(int N,int* target,int* source){
+	int i=blockDim.x*blockIdx.x+threadIdx.x;
+	if (i<N) target[i]=source[i];
 }
-__global__ void Memcpyadd(int* target,int*source,int add){
-	int i=threadIdx.x;
-	target[i]=source[i]+add;
+__global__ void Memcpyadd(int N,int* target,int*source,int add){
+	int i=blockDim.x*blockIdx.x+threadIdx.x;
+	if (i<N) target[i]=source[i]+add;
 }
 #define Mega 1000000
 #define Kilo 1000
+#define add 10
 int main(){
-	srand(time(NULL));
-	size_t N=100*Mega;
+	size_t N=10*Mega;
 	size_t size=N*sizeof(int);
 
 	cudaEvent_t start, stop;
@@ -24,12 +25,30 @@ int main(){
 	float t,r;
 	
 	int* c_a=(int*)malloc(size);
-	for(int i=0;i<N;i++){
-		c_a[i]=rand()%10000;
+	#pragma omp parallel
+	{
+		srand(int(time(NULL)^omp_get_thread_num()));
+		#pragma omp for
+		for(int i=0;i<N;i++){
+			c_a[i]=rand();
+		}
 	}	
 	int* d_a;
 	cudaMalloc(&d_a,size);
+	int *c_d=(int*)malloc(size);
+	cudaEventRecord(start);
 
+	#pragma omp parallel for
+	for(int i=0;i<N;i++){
+		c_d[i]=c_a[i]+add;
+	}
+
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&t, start, stop);
+	t/=1000.0;
+	r=(size/(1000.0*Mega))/t;
+	printf("copy in Ram time: %f,rate: %f GB/s\n",t,r);
 	cudaEventRecord(start);
 
 	//copy c_a to Device
@@ -51,10 +70,9 @@ int main(){
 
 	cudaEventRecord(start);
 	//copy d_a in d_b
-	cudaMemcpy(d_b,d_a,size,cudaMemcpyDeviceToDevice);
-
-	//Memcpy<<<1,N>>>(d_b,d_a);
-
+	//cudaMemcpy(d_b,d_a,size,cudaMemcpyDeviceToDevice);
+	Memcpyadd<<<N/1000,1000>>>(N,d_b,d_a,add);
+	cudaDeviceSynchronize();
 	cudaEventRecord(stop);
 	cudaEventSynchronize(stop);
 	cudaEventElapsedTime(&t, start, stop);
@@ -82,7 +100,7 @@ int main(){
 		}
    	}
    	for(int i=0;i<N;i++){
-   		if(c_c[i]!=c_a[i]){
+   		if(c_c[i]!=c_d[i]){
 		printf("error in c_c at %d\n",i);
 		break;
 		}
